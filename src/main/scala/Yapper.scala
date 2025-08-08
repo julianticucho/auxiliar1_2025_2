f0f2033dfe93e03272198f0cd80c7cc224eaf7f2
Julian Proboste <proboste80@gmail.com>
2025-08-08 17:39:02 -0400

    otro commit

--- src/main/scala/Yapper.scala
+++ src/main/scala/Yapper.scala
@@ -3,7 +3,7 @@ object Yapper {
   val greeting:String=""
   val farewell:String=""
   def main(args: Array[String]): Unit = {
-    println("CAMBIOSSS")
+    println("CAaMBIOSSS")
   }
 
   def say(something:String):Unit={
